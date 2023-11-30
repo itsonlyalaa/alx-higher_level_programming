9b0d725282affa610f5f1feaf3f9a5d8f5185213
Alaa Okair <alaaokair330@gmail.com>
2023-11-30 11:55:56 -0800

    task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+argcs = len(sys.argv) - 1
+
+x = 0
+result = 0
+for arg in sys.argv:
+    if x != 0:
+        result += int(arg)
+    else:
+        x += 1
+print("{:d}".format(result))
