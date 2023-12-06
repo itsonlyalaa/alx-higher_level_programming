7f8d7d8501fe7d2b921729abf1869062220a18ba
Alaa Okair <alaaokair330@gmail.com>
2023-12-06 14:35:13 -0800

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                            'C': 100, 'D': 500, 'M': 1000}
+        res = 0
+        items = 0
+        roman = roman_string
+        if type(roman) is not str or len(roman) == 0:
+                return 0
+        for items in range(items, len(roman)):
+                if items < len(roman) - 1\
+                   and\
+                   roman_dict[roman[items]] <\
+                   roman_dict[roman[items + 1]]:
+                    res -= roman_dict[roman[items]]
+                else:
+                    res += roman_dict[roman[items]]
+        return res
