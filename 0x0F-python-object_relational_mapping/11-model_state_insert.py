41cdd44b5eb1fec69b70b1a64b8eb1139aa65fc9
Alaa Okair <alaaokair330@gmail.com>
2024-04-14 16:17:23 -0700

    tasks

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -10,7 +10,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    
+
     db_ur = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
     engine = create_engine(db_ur)
